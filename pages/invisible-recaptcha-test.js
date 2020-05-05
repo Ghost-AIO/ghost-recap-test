f73e164af36b114a3ca0d793719ca7113fab6869
JM <28718610+jm3466@users.noreply.github.com>
2020-05-05 13:18:03 -0700

    Fix invisible recaptcha page + Add statusMessage

--- pages/invisible-recaptcha-test.js
+++ pages/invisible-recaptcha-test.js
@@ -1,27 +1,64 @@
 import React, { Component } from 'react';
 import { invisible } from '../utils/keys';
 import dynamic from 'next/dynamic';
-
-const ReCAPTCHA = dynamic(() => import('react-google-recaptcha'))
-// import ReCAPTCHA from 'react-google-recaptcha';
+import Router from 'next/router';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 const recaptchaRef = React.createRef();
-function RecaptchaTest({ sitekey }) {
-    return (
+
+class InvisibleRecaptcha extends Component {
+
+  constructor(props) {
+    super(props);
+    this.props = props;
+
+    this.state = {
+      statusMessage:"Click the Test reCAPTCHA button to test invisble reCAPTCHA"
+    }
+  }
+  testInvisible(e) {
+
+    e.preventDefault();
+
+    this.setState({
+      statusMessage:"Testing invisible reCAPTCHA now"
+    });
+
+    return setTimeout(() => {
+      recaptchaRef.current.execute();
+    }, 500);
+  
+  
+  }
+  onChange(value) {
+    console.log(value);
+
+    this.setState({ 
+      statusMessage:"Test Completed"
+    });
+  }
+  render() {
+      return (
         <div>
             <h2>Google Invisible reCAPTCHA test</h2>
-            <form onSubmit={() => { recaptchaRef.current.execute(); }}>
+            <form onSubmit={this.testInvisible.bind(this)}>
+              <h3>{this.state.statusMessage}</h3>
             <ReCAPTCHA
+                onChange={this.onChange.bind(this)}
                 ref={recaptchaRef}
-                sitekey={sitekey}
+                sitekey={this.props.sitekey}
                 size="invisible"
                 badge="inline"
             />
-            <button id="test" onClick={() => { recaptchaRef.current.execute(); }}>Test Recaptcha</button>
+            <br></br>
+            <button id="test" type="submit">Test reCAPTCHA</button>
             </form>
+            <br></br>
+            <button onClick={() => Router.reload()}>Reset</button>
         </div>
     )
   }
+}
 
 export async function getStaticProps() {
 
@@ -34,4 +71,6 @@ export async function getStaticProps() {
     }
 }
 
-export default RecaptchaTest
+export default dynamic(() => Promise.resolve(InvisibleRecaptcha), {
+  ssr: false
+});
